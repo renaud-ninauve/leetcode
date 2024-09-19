9aca1767df0600dbd9a3f36e31ecb32d3df4fcfd
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-09-19 09:16:36 +0200

    new solution

--- src/main/java/fr/ninauve/renaud/leetcode/trappingrain/Solution.java
+++ src/main/java/fr/ninauve/renaud/leetcode/trappingrain/Solution.java
@@ -6,50 +6,20 @@ import java.util.stream.IntStream;
 // https://leetcode.com/problems/trapping-rain-water/
 public class Solution {
 
-    public int trap(int[] height) {
-        int maxHeight = Arrays.stream(height).max().orElse(0);
-        return (int) IntStream.rangeClosed(1, maxHeight)
-                .mapToLong(currentHeight -> trapAtHeight(height, currentHeight))
-                .sum();
-    }
-
-    private int trapAtHeight(int[] height, int currentHeight) {
-        TrapAtHeightContext ctx = new TrapAtHeightContext(currentHeight);
-
-        for(int columnHeight: height) {
-            if (!ctx.left) {
-                handleNothingAtLeft(ctx, columnHeight);
-            } else {
-                handleSomethingAtLeft(ctx, columnHeight);
+    public int trap(int[] heights) {
+        boolean first = true;
+        int previous = 0;
+        int retainedWater = 0;
+        for(int height: heights) {
+            if (first) {
+                first = false;
+                previous = height;
+                continue;
+            }
+            if (height < previous) {
+                retainedWater += previous - height;
             }
         }
-        return ctx.trapped;
-    }
-
-    private void handleNothingAtLeft(TrapAtHeightContext ctx, int columnHeight) {
-        if (columnHeight >= ctx.currentHeight) {
-            ctx.left = true;
-            ctx.emptyAfterLeft = 0;
-        }
-    }
-
-    private void handleSomethingAtLeft(TrapAtHeightContext ctx, int columnHeight) {
-        if (columnHeight >= ctx.currentHeight) {
-            ctx.trapped += ctx.emptyAfterLeft;
-            ctx.emptyAfterLeft = 0;
-        } else {
-            ctx.emptyAfterLeft++;
-        }
-    }
-
-    private static class TrapAtHeightContext {
-        private final int currentHeight;
-        private boolean left = false;
-        private int emptyAfterLeft = 0;
-        private int trapped = 0;
-
-        private TrapAtHeightContext(int currentHeight) {
-            this.currentHeight = currentHeight;
-        }
+        return retainedWater;
     }
 }
--- src/test/java/fr/ninauve/renaud/leetcode/trappingrain/SolutionTest.java
+++ src/test/java/fr/ninauve/renaud/leetcode/trappingrain/SolutionTest.java
@@ -1,39 +1,75 @@
 package fr.ninauve.renaud.leetcode.trappingrain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
 
-    @Test
-    void should_trap_nothing_when_no_heights() {
-        int actual = new Solution().trap(new int[0]);
-        assertThat(actual).isZero();
+    static Stream<Arguments> should_trap_water() {
+        return Stream.of(Arguments.of(
+                """
+                        ..........
+                        ..........
+                        ..........
+                        ..........
+                        """,
+                0
+        ),
+                Arguments.of(
+                        """
+                                ..........
+                                #........#
+                                #........#
+                                ##########
+                                """,
+                        16
+                ));
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void should_trap_water(String heightsString, int expected) {
+        int[] heights = parseHeights(heightsString);
+
+        int actual = new Solution().trap(heights);
+
+        assertThat(actual).isEqualTo(expected);
     }
 
     @Test
-    void should_trap_nothing_when_heights_are_zeros() {
-        int actual = new Solution().trap(new int[] {0, 0, 0});
-        assertThat(actual).isZero();
+    void parse_heights() {
+        int[] actual = parseHeights("""
+                ..........
+                .......#..
+                ....#..#..
+                .#.######.
+                """
+        );
+
+        assertThat(actual).containsExactly(0, 1, 0, 1, 2, 1, 1, 3, 1, 0);
     }
 
-    @Test
-    void should_trap_in_height_0_when_enclosed_by_height_one() {
-        int actual = new Solution().trap(new int[] {0, 1, 0, 1, 0, 0, 1, 0});
-        assertThat(actual).isEqualTo(3);
-    }
-
-    @Test
-    void should_trap_when_several_heights1() {
-        int actual = new Solution().trap(new int[] {0,1,0,2,1,0,1,3,2,1,2,1});
-        assertThat(actual).isEqualTo(6);
-    }
-
-    @Test
-    void should_trap_when_several_heights2() {
-        int actual = new Solution().trap(new int[] {4,2,0,3,2,5});
-        assertThat(actual).isEqualTo(9);
+    private int[] parseHeights(String heightsString) {
+        List<Integer> heightsList = heightsString.lines()
+                .map(l -> l.chars().mapToObj(c -> c == '#' ? 1 : 0).toList())
+                .reduce(List.of(), (a, b) -> {
+                    if (a.isEmpty()) {
+                        return b;
+                    }
+                    final List<Integer> sum = new ArrayList<>(a.size());
+                    for (int i = 0; i < a.size(); i++) {
+                        sum.add(a.get(i) + b.get(i));
+                    }
+                    return sum;
+                });
+        return heightsList.stream().mapToInt(i -> i).toArray();
     }
 }
