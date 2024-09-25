b2537a3fe8547ca5e95ad9890954d955a8600ea8
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-09-25 15:30:21 +0200

    longest parentheses

---
+++ src/main/java/fr/ninauve/renaud/leetcode/longestparentheses/LongestParentheses.java
@@ -0,0 +1,8 @@
+package fr.ninauve.renaud.leetcode.longestparentheses;
+
+// https://leetcode.com/problems/longest-valid-parentheses/
+public class LongestParentheses {
+    public int longestValidParentheses(String s) {
+        return 0;
+    }
+}
---
+++ src/test/java/fr/ninauve/renaud/leetcode/longestparentheses/LongestParenthesesTest.java
@@ -0,0 +1,27 @@
+package fr.ninauve.renaud.leetcode.longestparentheses;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LongestParenthesesTest {
+
+    static Stream<Arguments> longestValidParentheses() {
+        return Stream.of(
+                Arguments.of(
+                        "",
+                        0
+                ));
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void longestValidParentheses(String str, int expected) {
+        int actual = new LongestParentheses().longestValidParentheses(str);
+        assertThat(actual).isEqualTo(expected);
+    }
+}
