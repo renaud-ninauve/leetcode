c456a2e84eae551c15a9e656031bbd6c04ed8b33
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-09-30 15:14:48 +0200

    SubsequenceMatcher

--- src/main/java/fr/ninauve/renaud/leetcode/wildcardmatching/WildCardMatching.java
+++ src/main/java/fr/ninauve/renaud/leetcode/wildcardmatching/WildCardMatching.java
@@ -12,16 +12,44 @@ public class WildCardMatching {
         if (pattern.indexOf(ANY_SUBSEQUENCE) < 0 && string.length() != pattern.length()) {
             return false;
         }
+        SubsequenceMatcher matcher = new SubsequenceMatcher(pattern);
         for (int i = 0; i < string.length(); i++) {
             char actualChar = string.charAt(i);
-            char expectedChar = pattern.charAt(i);
-            if (expectedChar == ANY_CHAR || expectedChar == ANY_SUBSEQUENCE) {
-                continue;
-            }
-            if (actualChar != expectedChar) {
+            matcher.actualChar(actualChar);
+            if (!matcher.matches()) {
                 return false;
             }
         }
         return true;
+    }
+
+    private static class SubsequenceMatcher {
+        private final String pattern;
+        private int matchLength = 0;
+
+        private SubsequenceMatcher(String pattern) {
+            this.pattern = pattern;
+        }
+
+        private void actualChar(char actual) {
+            if (matchLength < 0 || matchLength >= pattern.length()) {
+                return;
+            }
+            char expected = pattern.charAt(matchLength);
+            boolean matches = expected == ANY_CHAR || actual == expected;
+            if (matches) {
+                matchLength++;
+            } else {
+                matchLength = -1;
+            }
+        }
+
+        private boolean matches() {
+            return matchLength >= 0;
+        }
+
+        private int getMatchLength() {
+            return matchLength;
+        }
     }
 }
