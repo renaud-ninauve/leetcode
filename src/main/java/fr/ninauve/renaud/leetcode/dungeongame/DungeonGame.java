0b7c125218adc21f7e99d461b016e541e13b70b2
renaud.ninauve <renaud.ninauve.1@chanel.com>
2026-06-17 10:53:09 +0200

    track min end life only

--- src/main/java/fr/ninauve/renaud/leetcode/dungeongame/DungeonGame.java
+++ src/main/java/fr/ninauve/renaud/leetcode/dungeongame/DungeonGame.java
@@ -1,29 +1,31 @@
 package fr.ninauve.renaud.leetcode.dungeongame;
 
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Map;
 import java.util.Queue;
 
 public class DungeonGame {
     public int calculateMinimumHP(int[][] dungeon) {
-        final Map<Room, Integer> maxMinLifeValues = new HashMap<>();
+        int maxMinLife = Integer.MIN_VALUE;
         final Queue<ToVisit> queue = new LinkedList<>();
         queue.add(new ToVisit(new Room(new Row(0), new Col(0)), 0, 0));
         final Row maxRow = new Row(dungeon.length - 1);
         final Col maxCol = new Col(dungeon[0].length - 1);
+        final Room endRoom = new Room(maxRow, maxCol);
         while(!queue.isEmpty()) {
             ToVisit toVisit = queue.poll();
             Room room = toVisit.room();
+            int life = dungeon[room.row().value()][room.col().value()];
             int lifeTotal = toVisit.lifeTotal();
             int minLifeTotal = toVisit.minLifeTotal();
-            int life = dungeon[room.row().value()][room.col().value()];
+
             int newLifeTotal = lifeTotal + life;
             int newMinLifeTotal = Math.min(minLifeTotal, newLifeTotal);
-            Integer maxMinValue = maxMinLifeValues.get(room);
-            if (maxMinValue == null || newMinLifeTotal > maxMinValue) {
-                maxMinLifeValues.put(room, newMinLifeTotal);
+
+            if (room.equals(endRoom)) {
+                maxMinLife = Integer.max(newMinLifeTotal, maxMinLife);
+                continue;
             }
+
             Room right = room.right();
             if (right.isValid(maxRow, maxCol)) {
                 queue.offer(new ToVisit(right, newLifeTotal, newMinLifeTotal));
@@ -33,8 +35,7 @@ public class DungeonGame {
                 queue.offer(new ToVisit(down, newLifeTotal, newMinLifeTotal));
             }
         }
-        Integer endLifeTotal = maxMinLifeValues.get(new Room(maxRow, maxCol));
-        return endLifeTotal < 0 ? -endLifeTotal + 1 : 1;
+        return maxMinLife < 0 ? -maxMinLife + 1 : 1;
     }
 
     record ToVisit(Room room, int lifeTotal, int minLifeTotal) {}
