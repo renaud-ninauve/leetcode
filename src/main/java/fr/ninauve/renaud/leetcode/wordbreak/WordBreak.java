88123bf472a5f2070d72d167b6b7bbfcc0d1af6a
renaud.ninauve <renaud.ninauve.1@chanel.com>
2026-07-01 16:43:55 +0200

    wordbreak

--- src/main/java/fr/ninauve/renaud/leetcode/wordbreak/WordBreak.java
+++ src/main/java/fr/ninauve/renaud/leetcode/wordbreak/WordBreak.java
@@ -24,32 +24,26 @@ public class WordBreak {
         }
 
         boolean isComposedOfPrefixesOnly(String word) {
-            List<Completed> completed = new ArrayList<>();
-            List<NodeAndCompletedWords> nodes = new ArrayList<>();
-            nodes.add(new NodeAndCompletedWords(0, root, 0));
+            List<Node> nodes = new ArrayList<>();
+            nodes.add(root);
             for (int i = 0; i < word.length(); i++) {
                 char c = word.charAt(i);
                 nodes = nodes.stream()
                         .map(node -> node.getEdge(c))
                         .filter(Objects::nonNull)
                         .flatMap(node -> {
-                            if (!node.isWordEnd()) {
+                            if (!node.isWordEnd) {
                                 return Stream.of(node);
                             }
-                            int completedLength = node.getIndex() + 1;
-                            completed.add(new Completed(node.start, completedLength));
-                            NodeAndCompletedWords restart = new NodeAndCompletedWords(node.start + completedLength, root, completedLength);
                             return node.isEmpty()
-                                    ? Stream.of(restart)
-                                    : Stream.of(node, restart);
-                        }).toList();
+                                    ? Stream.of(root)
+                                    : Stream.of(node, root);
+                        }).distinct()
+                        .toList();
             }
             return nodes.stream()
-                    .anyMatch(node -> node.getIndex() == -1);
+                    .anyMatch(node -> node.index == -1);
         }
-    }
-
-    record Completed(int start, int length) {
     }
 
     static class Node {
@@ -65,45 +59,16 @@ public class WordBreak {
             return edges.computeIfAbsent(c, key -> new Node(index + 1));
         }
 
+        Node getEdge(Character c) {
+            return edges.get(c);
+        }
+
         void setWordEnd() {
             this.isWordEnd = true;
         }
 
         boolean isEmpty() {
             return edges.isEmpty();
-        }
-    }
-
-    static class NodeAndCompletedWords {
-        final int start;
-        final Node node;
-        final int completedLength;
-
-        public NodeAndCompletedWords(int start, Node node, int completedLength) {
-            this.start = start;
-            this.node = node;
-            this.completedLength = completedLength;
-        }
-
-        int getCompletedLength() {
-            return completedLength;
-        }
-
-        NodeAndCompletedWords getEdge(Character c) {
-            Node edge = node.edges.get(c);
-            return edge == null ? null : new NodeAndCompletedWords(start, edge, completedLength);
-        }
-
-        int getIndex() {
-            return node.index;
-        }
-
-        boolean isWordEnd() {
-            return node.isWordEnd;
-        }
-
-        boolean isEmpty() {
-            return node.isEmpty();
         }
     }
 }
--- src/test/java/fr/ninauve/renaud/leetcode/wordbreak/WordBreakTest.java
+++ src/test/java/fr/ninauve/renaud/leetcode/wordbreak/WordBreakTest.java
@@ -17,7 +17,8 @@ class WordBreakTest {
             "applepenapple [apple,pen] true",
             "catsandog [cats,dog,sand,and,cat] false",
             "aaaaaaa [aaaa,aaa] true",
-            "acaaaaabbbdbcccdcdaadcdccacbcccabbbbcdaaaaaadb [abbcbda,cbdaaa,b,dadaaad,dccbbbc,dccadd,ccbdbc,bbca,bacbcdd,a,bacb,cbc,adc,c,cbdbcad,cdbab,db,abbcdbd,bcb,bbdab,aa,bcadb,bacbcb,ca,dbdabdb,ccd,acbb,bdc,acbccd,d,cccdcda,dcbd,cbccacd,ac,cca,aaddc,dccac,ccdc,bbbbcda,ba,adbcadb,dca,abd,bdbb,ddadbad,badb,ab,aaaaa,acba,abbb] true"
+            "acaaaaabbbdbcccdcdaadcdccacbcccabbbbcdaaaaaadb [abbcbda,cbdaaa,b,dadaaad,dccbbbc,dccadd,ccbdbc,bbca,bacbcdd,a,bacb,cbc,adc,c,cbdbcad,cdbab,db,abbcdbd,bcb,bbdab,aa,bcadb,bacbcb,ca,dbdabdb,ccd,acbb,bdc,acbccd,d,cccdcda,dcbd,cbccacd,ac,cca,aaddc,dccac,ccdc,bbbbcda,ba,adbcadb,dca,abd,bdbb,ddadbad,badb,ab,aaaaa,acba,abbb] true",
+            "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab [a,aa,aaa,aaaa,aaaaa,aaaaaa,aaaaaaa,aaaaaaaa,aaaaaaaaa,aaaaaaaaaa] false"
     })
     void should_word_break(String s, String wordDictParam, boolean expected) {
         List<String> wordDict =
