db2952b384248dbd5dc3537e486b4f3c4de30ad3
renaud.ninauve <renaud.ninauve.1@chanel.com>
2026-07-02 09:23:46 +0200

    houserobber ok

--- src/main/java/fr/ninauve/renaud/leetcode/houserobber/HouseRobber.java
+++ src/main/java/fr/ninauve/renaud/leetcode/houserobber/HouseRobber.java
@@ -1,8 +1,5 @@
 package fr.ninauve.renaud.leetcode.houserobber;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class HouseRobber {
     public int rob(int[] nums) {
         if (nums.length == 1) {
@@ -11,28 +8,18 @@ public class HouseRobber {
         if (nums.length == 2) {
             return Math.max(nums[0], nums[1]);
         }
-        int lastHouse = nums.length - 1;
-        int max = 0;
-        Queue<Rob> queue = new LinkedList<>();
-        queue.offer(new Rob(0, nums[0]));
-        queue.offer(new Rob(1, nums[1]));
-        while(!queue.isEmpty()) {
-            Rob current = queue.poll();
-            if (current.house() >= lastHouse - 1) {
-                max = Math.max(current.totalRobbed(), max);
-                continue;
-            }
-            int next2 = current.house() + 2;
-            if (next2 <= lastHouse) {
-                queue.offer(new Rob(next2, current.totalRobbed() + nums[next2]));
-            }
-            int next3 = current.house() + 3;
-            if (next3 <= lastHouse) {
-                queue.offer(new Rob(next3, current.totalRobbed() + nums[next3]));
+        int[] max = new int[nums.length];
+        max[0] = nums[0];
+        max[1] = nums[1];
+        for (int i = 2; i < nums.length; i++) {
+            int num = nums[i];
+            if (i == 2) {
+                max[i] = num + max[i - 2];
+            } else {
+                max[i] = num + Math.max(max[i - 3], max[i - 2]);
             }
         }
-        return max;
-    }
 
-    record Rob(int house, int totalRobbed){}
+        return Math.max(max[nums.length - 2], max[nums.length - 1]);
+    }
 }
